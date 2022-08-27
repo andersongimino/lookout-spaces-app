eb8b67b9d3b1c30fc20a591d055e90065c18c2ea
Anderson Gimino <andersongimino@gmail.com>
2022-08-27 15:58:08 -0300

    Quick start

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titleTest}>Lookout spaces</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#1171ef",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    titleTest: {
+        fontSize: 18,
+        color: "#ffffff",
+        
+    },
 });
